ef4427d011966b27940958c3ce805845e046aced
bfitzgit23 <dfitz@me.com>
2022-04-26 18:27:38 +0000

    Comment : updated repo on Tue Apr 26 18:27:38 UTC 2022

---
+++ replctext2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# **************** Change Variables Here ************
+startdirectory="/var/lib/layman/kde-sunset"
+searchterm="cmake-utils"
+replaceterm="cmake"
+# **********************************************************
+
+echo "***************************************************"
+echo "* Search and Replace in Files Version 01-Aug-2012 *"
+echo "***************************************************"
+
+i=0; 
+
+  for file in $(grep -l -R $searchterm $startdirectory)
+    do
+      cp $file $file.bak
+      sed -e "s/$searchterm/$replaceterm/ig" $file > tempfile.tmp
+      mv tempfile.tmp $file
+
+    let i++;
+
+      echo "Modified: " $file
+    done
+
+echo " *** All Done! *** Modified files:" $i
